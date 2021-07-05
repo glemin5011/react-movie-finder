520f68d5be17061fe75d4dd2dd2af0ca03f4be0b
Matej <matejpale@gmail.com>
2021-07-05 22:30:39 +0200

    app built, struggling on passing error to parent

--- script.js
+++ script.js
@@ -5,24 +5,36 @@
 class MovieFinder extends React.Component {
   constructor(props) {
     super(props);
+    //Ref for error messages
+    this.childRef = React.createRef();
     this.state = {
       results: [],
+      error: "",
     };
+    this.addMovieResults = this.addMovieResults.bind(this);
+  }
+
+  addMovieResults(data, errorMessage) {
+    this.setState({ results: data, error: errorMessage });
   }
 
   render() {
-    const { results } = this.state;
+    const { results, error } = this.state;
     return (
       <React.Fragment>
         <div className="container-fluid">
           <h1 className="text-center">Movie Finder</h1>
           <div className="row">
-            <UserInput />
-            {results.map((movie) => {
-              return null; //returns nothing for now
-            })}
+            <UserInput onSubmit={this.addMovieResults} />
+            {(() => {
+              if (error) {
+                return error;
+              }
+              return results.map((movie) => {
+                return <Movie key={movie.imdbID} movie={movie} />;
+              });
+            })()}
           </div>
-          <div className="row"></div>
         </div>
       </React.Fragment>
     );
@@ -34,6 +46,7 @@ class UserInput extends React.Component {
     super(props);
     this.state = {
       searchTerm: "",
+      //error: "",
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -45,6 +58,36 @@ class UserInput extends React.Component {
 
   handleSubmit(event) {
     event.preventDefault();
+    let { searchTerm } = this.state;
+    searchTerm = searchTerm.trim(); // clean the string
+    if (!searchTerm) {
+      //if value is empty return nothing
+      return;
+    }
+
+    //AJAX request to OMDBAPI to get a list of results
+
+    fetch(`https://www.omdbapi.com/?s=${searchTerm}&apikey=61c1fa07`)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error("Request was either a 404 or 500");
+      })
+      .then((data) => {
+        if (data.Response === "False") {
+          throw new Error(data.Error);
+        }
+        if (data.Response === "True" && data.Search) {
+          this.props.onSubmit({ results: data.Search, error: "" });
+          //this.setState({ error: "" });
+        }
+      })
+      .catch((error) => {
+        //this.setState({ error: error.message });
+        this.props.onSubmit({ error: error.message });
+        console.log(error);
+      });
   }
   render() {
     const { searchTerm } = this.state;
@@ -67,23 +110,28 @@ class UserInput extends React.Component {
   }
 }
 
-class Movies extends React.Component {
+class Movie extends React.Component {
   constructor(props) {
     super(props);
   }
   render() {
+    const { Title, Year, imdbID, Type, Poster } = this.props.movie;
     return (
-      <React.Fragment>
-        <div className="col-6">
-          <img src="{image}" />
+      <div className="row">
+        <div className="col-4 col-md-3 mb-3">
+          <a href={`https://www.imdb.com/title/${imdbID}/`} target="_blank">
+            <img src={Poster} className="img-fluid" />
+          </a>
         </div>
-        <div className="col-6">
-          <h5>{movie}</h5>
-          <p>{type}</p>
-          <span> | </span>
-          <p>{year}</p>
+        <div className="col-8 col-md-9 mb-3">
+          <a href={`https://www.imdb.com/title/${imdbID}/`} target="_blank">
+            <h4>{Title}</h4>
+            <p>
+              {Type} | {Year}
+            </p>
+          </a>
         </div>
-      </React.Fragment>
+      </div>
     );
   }
 }
